30279fbe3c7c6de1c76af821ead8ca256322e1b5
Eder <ederbronson@gmail.com>
2022-02-15 22:04:56 -0300

    Bateria Midi Finalizado

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio'){
+            elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0
-
-//enquanto
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-//template string
-    const idAudio = `#som_${instrumento}`;
-
-    //console.log(idAudio);
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function (){
         tocaSom(idAudio);
     }
 
-    contador = (contador + 1);
+    tecla.onkeydown = function (evento){
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+        
+    }
 
-    //console.log(contador);
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
 }
+
+
